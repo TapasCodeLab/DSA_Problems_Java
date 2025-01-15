68c8a15fbf5ee96e9ec13b770792a8bb1e279a40
Tapas Saha <165073726+TapasCodeLab@users.noreply.github.com>
2025-01-16 00:32:42 +0530

    init commit

---
+++ src/main/java/org/scaler/dsa_problems_java/DsaProblemsJavaApplication.java
@@ -0,0 +1,13 @@
+package org.scaler.dsa_problems_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaProblemsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaProblemsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/scaler/dsa_problems_java/DsaProblemsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.dsa_problems_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaProblemsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
